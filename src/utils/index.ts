47ff52856434caaac3aeee19f59e3dc5785157b9
Stefano Saffran <stefanosaffran@hotmail.com>
2020-10-06 20:25:58 +0200

    :sparkles: feat: add generate periods and format functions

---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export { default as formatNumber } from './format';
+export { default as generatePeriods } from './generate-periods';
+export { default as removeHashFromColor } from './remove-hash-from-color';
